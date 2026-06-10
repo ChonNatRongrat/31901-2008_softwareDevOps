8c183e44d7a0f510877629a28ce8b522b085e0a9
ChonNatRongrat <68319010005@htc.ac.th>
2026-06-10 12:53:45 +0700

    Create my-component.js

---
+++ my-component.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+export default function MyComponent() {
+  return 
+  <view>
+    <Text>
+      My Component : แอพนี้สร้างโดย นาย ณัฎฐ์ รงค์รัตน์
+    </Text>
+  </view>
+}
